dc94d3a5ad54bca74dc32aa9054a4554c129f53e
Abhishekkashyap007 <ak555585955@gmail.com>
2025-03-29 23:23:56 +0530

    change

--- routes/route.js
+++ routes/route.js
@@ -4,22 +4,17 @@ const myapp = express.Router();
 
 
 
-// myapp.get("/", (req, res) => {
-//     res.send("<h1>welcome to expressjs</h1>");
-// });
+myapp.get("/", (req, res) => {
+    res.send("<h1>welcome to expressjs</h1>");
+});
 
-// myapp.get("/about", (req, res) => {
-//     res.send("welcome");
-// });
+myapp.get("/about", (req, res) => {
+    res.send("welcome");
+});
 
-// myapp.get("/contact", (req, res) => {
-//     res.send(req.path);
-// });
-
-// myapp.get(/b/, (req, res) => {
-//     // res.send("ddfjsfk");
-//     res.json({ name: "abhi", age: "50" })
-// });
+myapp.get("/contact", (req, res) => {
+    res.send(req.path);
+});
 
 myapp.get("/alldata", async (req, res) => {
     const alldatalist = await myschimatype.find();
